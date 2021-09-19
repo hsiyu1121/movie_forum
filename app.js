c8909bcd4f6390528ade49d2487fc505f3c7ef0a
hsiyu1121 <bluefish1121@gmail.com>
2021-09-19 09:53:29 +0800

    project basic setting

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+app.engine('hbs', exphbs({ 
+  defaultLayout: 'main', 
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride('_method'))
+
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`)
