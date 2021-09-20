677170ef695dba2d31f91e1e6961f33ae293d500
hsiyu1121 <bluefish1121@gmail.com>
2021-09-20 16:18:20 +0800

    add express-session middleware

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const port = 3000;
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-
+const flash = require('connect-flash');
+const session = require('express-session')
 
 app.engine('hbs', exphbs({ 
   defaultLayout: 'main', 
@@ -14,7 +15,18 @@ app.set('view engine', 'hbs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
+app.use(flash());
+app.use(session({
+  secret: 'movies',
+  resave: false,
+  saveUninitialized: true
+}))
 
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg'); 
+  res.locals.error_msg = req.flash('error_msg');
+  next()
+})
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
