e6dba41c97b3b1506483c5d397a029468993b996
hsiyu1121 <bluefish1121@gmail.com>
2021-09-21 19:29:15 +0800

    add authenticated and authenticateAdmin

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,27 @@ const movieController = require('../controllers/movieController')
 const userController = require('../controllers/userController')
 const passport = require('passport');
 
-module.exports = (app) => {
+
+module.exports = (app, passport) => {
+  const authenticate = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    return res.redirect('/signin')
+  }
+  const authenticateAdmin = (req, res, next) => {
+    if (req.isAuthenticated()) {
+      if(req.user.role) { return next() }
+      return res.redirect('/')
+    }
+    return res.redirect('/signin')
+  }
+
   app.get('/', (req, res) => { res.redirect('/movies') })
-  app.get('/movies', movieController.getMovies)
+  app.get('/movies', authenticate, movieController.getMovies)
 
   app.get('/admin', (req, res) => { res.redirect('/admin/movies') })
-  app.get('/admin/movies', adminController.getMovies)
+  app.get('/admin/movies', authenticateAdmin, adminController.getMovies)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
