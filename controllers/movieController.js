dd12ad09e5b9dc9f40469d6a9acd06ec4773d9a3
hsiyu1121 <bluefish1121@gmail.com>
2021-09-29 14:56:09 +0800

    Add movies index page

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -1,12 +1,19 @@
 const db = require('../models')
 const Movie = db.Movie
+const Category = db.Category
 
 const movieController = {
   getMovies: (req, res) => {
     try {
-      Movie.findAll({ raw: true, nested: true })
+      Movie.findAll({ raw: true, nested: true, include: [Category]})
         .then(movies => {
-          return res.render('movies')
+          const data = movies.map(r => {
+            r,
+            r.description = r.description.substring(0, 50)
+            r.title = r.title.substring(0, 30)
+            return r
+          })
+          return res.render('movies', { movies: data })
         })
     } catch (error) {
       req.flash('error_msg', error.toString())
