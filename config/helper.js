ef63f198a6f316c356bc8ff2af81cd154371d5e5
hsiyu1121 <bluefish1121@gmail.com>
2021-10-11 11:50:51 +0800

    modify movies page style

--- config/helper.js
+++ config/helper.js
@@ -1,18 +1,21 @@
-const moment = require('moment');
+const moment = require("moment");
 module.exports = {
-  ifCond: function(a, b, options) {
+  ifCond: function (a, b, options) {
     if (a === b) {
-      return options.fn(this)
+      return options.fn(this);
     }
-    return options.inverse(this)
+    return options.inverse(this);
   },
-  momentAgo: function(a) {
-    return moment(a).fromNow()
+  momentAgo: function (a) {
+    return moment(a).fromNow();
   },
-  momentDate: function(b) {
-    return moment(b , 'YYYY/MM/DD').format().slice(0, 10)
+  momentDate: function (b) {
+    return moment(b, "YYYY/MM/DD").format().slice(0, 10);
   },
-  strLength: function(arr) {
-    return arr.length > 20 ? arr.slice(0, 20) + '...' : arr
+  strLength: function (arr) {
+    return arr.length > 30 ? arr.slice(0, 30) + "..." : arr;
   },
-}
+  strDesc: function (arr) {
+    return arr.length > 90 ? arr.slice(0, 90) + "..." : arr;
+  },
+};
