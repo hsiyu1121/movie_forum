9a56af5f7531f8941119993bcec4fb425377fe5d
hsiyu1121 <bluefish1121@gmail.com>
2021-10-14 12:59:42 +0800

    modify users seeds

--- app.js
+++ app.js
@@ -1,47 +1,50 @@
-if(process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express')
+const express = require("express");
 const app = express();
-const port = 3000;
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
-const flash = require('connect-flash');
-const session = require('express-session');
-const passport = require('./config/passport');
+const port = process.env.PORT || 3000;
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
+const flash = require("connect-flash");
+const session = require("express-session");
+const passport = require("./config/passport");
 
-
-app.engine('hbs', exphbs({ 
-  defaultLayout: 'main', 
-  extname: '.hbs', 
-  helpers: require('./config/helper')
-}));
-app.set('view engine', 'hbs');
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
-app.use('/upload', express.static(__dirname + '/upload'))
-app.use(methodOverride('_method'));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    helpers: require("./config/helper"),
+  })
+);
+app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/upload", express.static(__dirname + "/upload"));
+app.use(methodOverride("_method"));
 app.use(flash());
-app.use(session({
-  secret: 'movies',
-  resave: false,
-  saveUninitialized: true
-}))
+app.use(
+  session({
+    secret: process.env.SESSION_ID,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg'); 
-  res.locals.error_msg = req.flash('error_msg');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
   res.locals.user = req.user;
-  next()
-})
+  next();
+});
 
 app.listen(port, () => {
   console.log(`Express server running on port ${port}`);
-})
+});
 
-
-require('./routes')(app, passport);
+require("./routes")(app, passport);
 module.exports = app;
