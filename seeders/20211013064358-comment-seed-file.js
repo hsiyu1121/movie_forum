5b4338f88f1cd28853b22daef205919657d6d7cd
hsiyu1121 <bluefish1121@gmail.com>
2021-10-17 20:52:35 +0800

    modify comment seeds

--- seeders/20211013064358-comment-seed-file.js
+++ seeders/20211013064358-comment-seed-file.js
@@ -5,13 +5,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {   
     await queryInterface.bulkInsert('Comments', 
       Array.from({ length: 100 }).map((d, i) => ({
-        text: faker.lorem.paragraph();,
+        text: faker.lorem.paragraph(),
         UserId: Math.floor(Math.random() * 6) + 1,
         MovieId:  Math.floor(Math.random() * 80) + 1,
       }), {})
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Comments', null, {});
+    await queryInterface.bulkDelete('Comments', null, {})
   }
 };
