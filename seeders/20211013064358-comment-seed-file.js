e63f5a7c2f4e0f3abf11291ab1532eaf3044aab4
hsiyu1121 <bluefish1121@gmail.com>
2021-10-17 12:09:44 +0800

    modify config/config.json

--- seeders/20211013064358-comment-seed-file.js
+++ seeders/20211013064358-comment-seed-file.js
@@ -8,7 +8,7 @@ module.exports = {
         text: faker.lorem.paragraph();,
         UserId: Math.floor(Math.random() * 6) + 1,
         MovieId:  Math.floor(Math.random() * 80) + 1,
-      }), {});
+      }), {})
   },
 
   down: async (queryInterface, Sequelize) => {
